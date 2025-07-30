fd1652a6b869762c662507cb90fc12ff162db775
mahdihc222 <mahdihc222@gmail.com>
2025-07-30 22:44:30 +0600

    icon added

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("DSA Visualizer");
         //primaryStage.setFullScreen(true);
+        primaryStage.getIcons().add(new javafx.scene.image.Image(Main.class.getResourceAsStream("/Images/" + "icon" + ".png")));
         primaryStage.setMaximized(true);
         Scene scene = new Scene(HomePageUp.getView(primaryStage));
         primaryStage.setScene(scene);
